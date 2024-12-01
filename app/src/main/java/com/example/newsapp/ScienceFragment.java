b4fe564ee7bbe13c224dc9b4179f4c884621802f
Rohit Rajendra Zirmute <rohitzirmute821@gmail.com>
2024-12-01 16:41:49 +0530

    Update ScienceFragment.java

--- app/src/main/java/com/example/newsapp/ScienceFragment.java
+++ app/src/main/java/com/example/newsapp/ScienceFragment.java
@@ -20,7 +20,7 @@ import retrofit2.Response;
 
 public class ScienceFragment extends Fragment {
 
-    String api = "d0e7361fe3d64640b011faa36cc7f366";
+    String api = "";
     ArrayList<ModalClass> modelClassArrayList;
     Adapter adapter;
     String country = "in";
